3c517d65bd7a44f698b0066cc2b5fabda37d203e
Yichen Huang <achen765223419@gmail.com>
2022-10-12 02:28:32 -0700

    Add food ingredient sections with css

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 // styles
 import './Create.css'
@@ -8,10 +8,26 @@ export default function Create() {
 	const [title, setTitle] = useState("")
 	const [method, setMethod] = useState("")
 	const [cookingTime, setCookingTime] = useState("")
+	const [newIngredient, setNewIngredient] = useState("")
+	const [ingredients, setIngredients] = useState([])
+	const ingredientInput = useRef(null)
+
+	const handleAdd = (e) => {
+		e.preventDefault()
+		const ing = newIngredient.trim()
+
+		// check if duplicate
+		if (ing && !ingredients.includes(ing)) {
+			setIngredients(prevIngredients => [...prevIngredients, ing])
+		}
+
+		setNewIngredient("")
+		ingredientInput.current.focus()
+	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
-		console.log(title, method, cookingTime)
+		console.log(title, method, cookingTime, ingredients)
 	}
 
 	return (
@@ -30,7 +46,19 @@ export default function Create() {
 					/>
 				</label>
 
-				{/* ingredients go here*/}
+				<label>
+					<span>Recipe ingredients:</span>
+					<div className="ingredients">
+						<input
+							type="text"
+							onChange={e => setNewIngredient(e.target.value)}
+							value={newIngredient}
+							ref={ingredientInput}
+						/>
+						<button onClick={handleAdd} className="btn">add</button>
+					</div>
+				</label>
+				<p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)}</p>
 
 				<label>
 					<span>Recipe method: </span>
