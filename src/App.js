d0eba4e4d15161881c49ddec8d8830b72fe6ba04
Yichen Huang <achen765223419@gmail.com>
2022-10-11 21:32:03 -0700

    Add several web pages: home, recipe, search, and create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+// styles
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/recipes/:id'>
+            <Recipe />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
