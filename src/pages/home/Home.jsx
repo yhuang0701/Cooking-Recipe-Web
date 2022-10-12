6b62258c23a2f26863e28e7515cb99c4a8ed6e93
Yichen Huang <achen765223419@gmail.com>
2022-10-11 22:26:53 -0700

    Fetch data from db.json file and render on the Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,19 @@
+import { useFetch } from '../../hooks/useFetch'
+
+// styles
 import './Home.css'
 
 export default function Home() {
-    return (
-        <div>Home</div>
-    )
+  const { data: recipes, isPending, error } = useFetch("http://localhost:3000/recipes")
+
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes && recipes.map((recipe) => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
+    </div>
+  )
 }
