3afcc3100707179ac9d921a7b14bd0bdfa422420
Yichen Huang <achen765223419@gmail.com>
2022-10-19 02:06:21 -0700

    Wrap the whole application using the theme context provider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 import Searchbar from './Searchbar'
+import { useTheme } from '../hooks/useTheme'
 
 // styles
 import './Navbar.css'
 
+
 export default function Navbar() {
+
+  const { color } = useTheme()
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to='/' className="brand">
           <h1>Cooking King</h1>
