0810957dac59a2d6e5300a1db21232b28eab0451
Yichen Huang <achen765223419@gmail.com>
2022-10-19 18:44:32 -0700

    Fetch individual recipe data from firebase backend site

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
 import { useTheme } from '../../hooks/useTheme'
+import { projectFirestore } from '../../firebase/config'
 
 // styles
 import './Recipe.css'
@@ -8,9 +10,35 @@ import './Recipe.css'
 export default function Recipe() {
 
   const { id } = useParams()
-  const url = 'http://localhost:3000/recipes/' + id
-  const { error, isPending, data: recipe } = useFetch(url);
+  // const url = 'http://localhost:3000/recipes/' + id
+  // const { error, isPending, data: recipe } = useFetch(url);
   const { mode } = useTheme()
+  const [recipe, setRecipe] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    setIsPending(true)
+
+    projectFirestore
+      .collection('recipes')
+      .doc(id)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          setIsPending(false)
+          setRecipe(doc.data())
+        } else {
+          setIsPending(false)
+          setError("Could not find this recipe")
+        }
+      })
+      .catch(err => {
+        setError(err.message)
+        setIsPending(false)
+      })
+
+  }, [id])
 
   return (
     <div className={`recipe ${mode}`}>
