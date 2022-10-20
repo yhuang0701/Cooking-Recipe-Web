855c5340da4899f671ca5298698f8bdb3e8d0a81
Yichen Huang <achen765223419@gmail.com>
2022-10-19 19:17:05 -0700

    add delete functionality on recipe listings

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom'
 import { useTheme } from '../hooks/useTheme'
+import deleteIcon from '../assets/delete-icon.svg'
+import { projectFirestore } from '../firebase/config'
 
 // styles
 import './RecipeList.css'
@@ -12,6 +14,18 @@ export default function RecipeList({ recipes }) {
     return <div className="error">No recipes to load...</div>
   }
 
+  const handleClick = (id) => {
+    try {
+      projectFirestore
+        .collection("recipes")
+        .doc(id)
+        .delete()
+    } catch (err) {
+      console.log(err.message);
+    }
+
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
@@ -20,6 +34,12 @@ export default function RecipeList({ recipes }) {
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
+          <img
+            className="delete"
+            src={deleteIcon}
+            alt="recipe delete icon"
+            onClick={() => { handleClick(recipe.id) }}
+          />
         </div>
       ))}
     </div>
